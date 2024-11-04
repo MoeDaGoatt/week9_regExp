28b74a73bbd073e9ae28632d3f35c4275645e462
Muhammad <legandgogeta@gmail.com>
2024-11-04 10:48:35 -0500

    Add x var

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
         final String regex = "[abc]";
         final String string = "The Bachelor of Science in Artificial Intelligence Management is designed to meet both transfer and continuing education needs for students, preparing them for a wide range of careers in the emerging field of AI. This program builds a foundation of both managerial and technical skills by combining business and computer science to understand, develop and manage AI applications. In particular, the program builds technical skills in machine learning and management, enabling the student to apply those skills in areas such as marketing, finance, supply chain, and other business functions. In addition, with courses on ethics and the societal implications of AI, the program reflects perspectives that are both diverse and international. The program is ideal for students with backgrounds in computer science and interested in learning more about the application of AI in business, and for students with backgrounds in business and interested in learning more about the technical aspects of AI. 10-20-2024, R02339482\n\n";
         final String subst = "X";
+        int x=8;
 
         final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         final Matcher matcher = pattern.matcher(string);
